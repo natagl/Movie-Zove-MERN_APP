eaecdcc0b10296d108699da6320ed22f25529d9c
natagl <glinska.nataliia21@gmail.com>
2020-05-12 18:16:28 -0400

    getting API from www.themoviedb.org

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,11 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'ca2f039fb797b341f464f8d41321f2f6';
+
+
+export const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/';
+
+
+
+
